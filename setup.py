a0e624efa983d4c8fb2a51df115f6a4cf89cbdc8
ayodhyaw2002 <ayodhyaweerabahu@gmail.com>
2025-09-06 21:00:41 +0530

    the lib

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sinhala_translit_lib",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="Sinhala to English phonetic transliteration library",
+    author="Ayodhya",
+    author_email="ayodhyaweerabahu@gmail.com",
+    url="https://github.com/ayodhyaw2002/Akura_Py-library-.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
